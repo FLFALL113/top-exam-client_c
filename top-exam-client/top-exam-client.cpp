f9d5ee788e9664b760c0c9a3119d772d9fc410cf
main <vany11pr@yandex.ru>
2025-01-22 16:43:19 +0500

    Добавьте файлы проекта.

---
+++ top-exam-client/top-exam-client.cpp
@@ -0,0 +1,33 @@
+﻿#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+using boost::asio::ip::tcp;
+
+int main() {
+    try {
+        boost::asio::io_context io_context;
+
+        tcp::socket socket(io_context);
+        tcp::resolver resolver(io_context);
+        boost::asio::connect(socket, resolver.resolve("127.0.0.1", "12345"));
+
+        cout << "Enter message for the server: ";
+        string message;
+        getline(cin, message);
+
+        boost::asio::write(socket, boost::asio::buffer(message));
+
+        // Получение ответа от сервера
+        char buffer[1024];
+        size_t length = socket.read_some(boost::asio::buffer(buffer));
+        string response(buffer, length);
+
+        cout << "Response from server: " << response << endl;
+    }
+    catch (exception& e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
