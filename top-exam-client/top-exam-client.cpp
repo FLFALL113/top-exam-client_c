95b8cd5022c62a6061ad48d40b88e93e5968e9cd
main <vany11pr@yandex.ru>
2025-01-22 16:44:39 +0500

    init

--- top-exam-client/top-exam-client.cpp
+++ top-exam-client/top-exam-client.cpp
@@ -3,14 +3,14 @@
 #include <string>
 
 using boost::asio::ip::tcp;
-
+using namespace std;
 int main() {
     try {
         boost::asio::io_context io_context;
 
         tcp::socket socket(io_context);
         tcp::resolver resolver(io_context);
-        boost::asio::connect(socket, resolver.resolve("127.0.0.1", "12345"));
+        boost::asio::connect(socket, resolver.resolve("127.0.0.1", "60"));
 
         cout << "Enter message for the server: ";
         string message;
@@ -18,7 +18,6 @@ int main() {
 
         boost::asio::write(socket, boost::asio::buffer(message));
 
-        // Получение ответа от сервера
         char buffer[1024];
         size_t length = socket.read_some(boost::asio::buffer(buffer));
         string response(buffer, length);
