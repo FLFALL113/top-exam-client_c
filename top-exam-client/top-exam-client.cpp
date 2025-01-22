c68a9da725ef82419d2db09ded23a739014c606b
main <vany11pr@yandex.ru>
2025-01-22 18:37:03 +0500

    saves

--- top-exam-client/top-exam-client.cpp
+++ top-exam-client/top-exam-client.cpp
@@ -1,28 +1,43 @@
 ﻿#include <boost/asio.hpp>
 #include <iostream>
 #include <string>
+#include <nlohmann/json.hpp>
+
+#define PORT 60
+#define IP "127.0.0.1"
 
 using boost::asio::ip::tcp;
 using namespace std;
+using json = nlohmann::json;
+
+
 int main() {
+    setlocale(LC_ALL, "RU");
     try {
+#pragma region Connection
+        
         boost::asio::io_context io_context;
 
         tcp::socket socket(io_context);
         tcp::resolver resolver(io_context);
-        boost::asio::connect(socket, resolver.resolve("127.0.0.1", "60"));
+        boost::asio::connect(socket, resolver.resolve(IP, to_string(PORT)));
+        json request;
+        cout << "Введите свое имя: ";
+        string name;
+        getline(cin, name);
+        request["name"] = name;
+        
+        string request_str = request.dump();
 
-        cout << "Enter message for the server: ";
-        string message;
-        getline(cin, message);
-
-        boost::asio::write(socket, boost::asio::buffer(message));
+        boost::asio::write(socket, boost::asio::buffer(request_str + '\n'));
 
         char buffer[1024];
         size_t length = socket.read_some(boost::asio::buffer(buffer));
         string response(buffer, length);
 
-        cout << "Response from server: " << response << endl;
+        cout << "Ответ с сервера: " << response << endl;
+#pragma endregion
+
     }
     catch (exception& e) {
         cerr << "Error: " << e.what() << endl;
